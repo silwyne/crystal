c78bbad71fdd4438ac51fa31ec0f25d7e67e7380
Silwyne <scaleenor@gmail.com>
2025-06-14 17:14:52 +0330

    feat: added flatmap function

---
+++ pkg/functions/flatmap.go
@@ -0,0 +1,32 @@
+package functions
+
+import "sync"
+
+type FlatMapFunction func(interface{}) ([]interface{}, bool)
+
+type FlatMapTransformation struct {
+	Function FlatMapFunction
+}
+
+func (m FlatMapTransformation) Apply(data interface{}) ([]interface{}, bool) {
+	result, boolResult := m.Function(data)
+	return result, boolResult
+}
+
+func (m FlatMapTransformation) ExecuteTransformation(wg *sync.WaitGroup, source_channel chan interface{}) chan interface{} {
+	result_channel := make(chan interface{})
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for input := range source_channel {
+			flatten_data, ok := m.Apply(input)
+			if ok {
+				for _, data := range flatten_data {
+					result_channel <- data
+				}
+			}
+		}
+		close(result_channel)
+	}()
+	return result_channel
+}
