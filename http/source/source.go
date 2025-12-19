d90e969f8d7588adb186397726d00396d036f4ba
Silwyne <scaleenor@gmail.com>
2025-12-19 14:10:44 +0330

    fix: using datagenerator for rate limitation on http.source

--- http/source/source.go
+++ http/source/source.go
@@ -38,53 +38,31 @@ func NewHttpSource(url string, method string, headers map[string]string, timeout
 }
 
 func (hs *HttpSource) Apply(source_channel chan row.Row, result_channel chan row.Row) signal.Signal {
-	for {
-		select {
-		case <-hs.ctx.Done():
-			return signal.SUCCESS
-		default:
-			// Create HTTP request
-			req, err := http.NewRequestWithContext(hs.ctx, hs.method, hs.url, nil)
-			if err != nil {
-				log.Printf("Error creating HTTP request: %v\n", err)
-				return signal.FAILURE
-			}
-
-			// Add headers
-			for key, value := range hs.headers {
-				req.Header.Set(key, value)
-			}
-
-			// Send request
-			resp, err := hs.client.Do(req)
-			if err != nil {
-				log.Printf("Error sending HTTP request: %v\n", err)
-				// For polling, you might want to continue after error
-				// return signal.FAILURE
-				time.Sleep(hs.pollInterval)
-				continue
-			}
-
-			// Deserialize response
-			deserializedRow, err := hs.deserializer(resp)
-			if err != nil {
-				log.Printf("Error deserializing HTTP response: %v\n", err)
-				return signal.FAILURE
-			}
-
-			// Send row to result channel
-			result_channel <- deserializedRow
-
-			// If you want one-time fetch (not continuous polling), break here
-			// break
-
-			// Wait before next poll
-			time.Sleep(hs.pollInterval)
+	for range source_channel {
+		req, err := http.NewRequestWithContext(hs.ctx, hs.method, hs.url, nil)
+		if err != nil {
+			log.Printf("Error creating HTTP request: %v\n", err)
+			return signal.FAILURE
 		}
-	}
 
-	// If you break from the loop above, you'd return SUCCESS here
-	// return signal.SUCCESS
+		// Add headers
+		for key, value := range hs.headers {
+			req.Header.Set(key, value)
+		}
+
+		resp, err := hs.client.Do(req)
+		if err != nil {
+			log.Printf("Error sending HTTP request: %v\n", err)
+		}
+		deserializedRow, err := hs.deserializer(resp)
+		if err != nil {
+			log.Printf("Error deserializing HTTP response: %v\n", err)
+			return signal.FAILURE
+		}
+
+		result_channel <- deserializedRow
+	}
+	return signal.SUCCESS
 }
 
 func (k HttpSource) IsResultStateless() bool {
